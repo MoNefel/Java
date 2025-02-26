2f7b13c5e9bd8472bbe337e788964a3db10dfef8
Mohamed Akez Nefel <mohamed.akez@nefel.education>
2025-02-26 16:41:45 +0100

    Practice Assignment Daikichi Routes

---
+++ SpringIntro/Practice/DaikichiRoutes/daikichi/src/main/java/com/daikichi/controllers/DaikichiController.java
@@ -0,0 +1,24 @@
+package com.daikichi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DaikichiController {
+	
+	@GetMapping("/daikichi")
+	public String welcome() {
+		return "Welcome!!";
+	}
+	
+	@GetMapping("/daikichi/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@GetMapping("/daikichi/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
