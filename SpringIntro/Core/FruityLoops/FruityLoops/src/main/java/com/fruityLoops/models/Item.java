d290246f8324be9251365dd768a5eca75214afe0
Mohamed Akez Nefel <mohamed.akez@nefel.education>
2025-02-27 15:07:57 +0100

    Core Assignments Fruity Loops

---
+++ SpringIntro/Core/FruityLoops/FruityLoops/src/main/java/com/fruityLoops/models/Item.java
@@ -0,0 +1,39 @@
+package com.fruityLoops.models;
+
+public class Item {
+	
+	private String name;
+	private double price;
+	
+	//Constructor
+	
+	public Item(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	
+	//Getters and setters
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+
+}
